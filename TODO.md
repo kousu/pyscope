9447f168a2be015ff8d8c109e4a34769c18accc7
kousu <nick@kousu.ca>
2013-12-22 03:00:59 -0500

    TODOs

---
+++ TODO.md
@@ -0,0 +1,12 @@
+# TODO
+
+Remember that a TODO doc is a living document:
+this is not a _spec_; you should edit this freely as ideas come up
+and let it help you shape your ideas.
+
+* [ ] Configurable scope window (controllable by, say, matplotlib widgets?)
+* [ ] Commandline sound source option
+* [ ] Reporting of underruns to the GUI
+* [ ] Figure out (and write up) the intricacies of matplotlib interactive vs noninteractive mode:
+  * **why is it so hard to just reliably make an empty plot and put data on it? Why does how that works change depending on environment?**
+  * see: [[minimal.py]]
---
+++ minimal.py
@@ -0,0 +1,20 @@
+# matplotlib is too helpful for its own good
+
+# IF I run this with 'python'
+# because the plot must be hooked to run when python exits but then immediately exits
+# IF I run it with 'python -i' then it does show up
+# 
+# IF i run this code with plt.ion()
+# paste this code into a terminal:
+
+# If I paste this into a terminal...
+
+import matplotlib.pyplot as plt
+#plt.ion() #key? does this show up???
+          # arrrrgh. 
+	  # WHY can't I just fucking MAKE A FUCKING PLOT FUCK JESUS
+ # the distinction between ion vs -i vs noninteractive python and arrrhghghghghg
+	  
+fig, ax = plt.subplots()
+
+#plt.show()
